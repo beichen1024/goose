788ce3a2f37509728ee4338af991e813005de1fd
Beibei Chen <beibei@formbio.com>
2024-04-08 17:31:42 -0500

    rm sql file

--- cmd/goose/migrations/1_init.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-create table hello (id integer);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-drop table hello;
--- +goose StatementEnd
